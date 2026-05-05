d6de83f2f5118ba1712cc3fcef841f30005b4ee5
Ashish <ashish@gmail.com>
2026-05-05 17:50:40 +0530

    add output

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,4 @@
+// Props (Properties) are used to pass data from one component to another component.
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
@@ -7,3 +8,5 @@ function App() {
 }
 
 export default App;
+// Output:
+// Hello, Rahul
