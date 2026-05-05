c24fb8fb5af1690f3b5655fc518f130eb979d36e
Ashish <ashish@gmail.com>
2026-05-05 17:54:41 +0530

    state

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,38 @@
-// Props (Properties) are used to pass data from one component to another component.
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+// // Props (Properties) are used to pass data from one component to another component.
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
 
-function App() {
-  return <Welcome name="Rahul" />;
-}
+// function App() {
+//   return <Welcome name="Rahul" />;
+// }
 
-export default App;
-// Output:
-// Hello, Rahul
+// export default App;
+// // Output:
+// // Hello, Rahul
+
+
+
+
+// State:-
+// State is used to store data inside a component.
+// When state changes, the component updates automatically.
+// import React, { Component } from "react";
+
+// class Counter extends Component {
+//   state = {
+//     count: 0
+//   };
+
+//   render() {
+//     return <h1>{this.state.count}</h1>;
+//   }
+// }
+
+// export default Counter;
+// // Output:
+// // 0
+
+
+
+
